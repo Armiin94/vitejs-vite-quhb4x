4084e3efa2be22962149c8b124a8d4d90f93ea93
Armiin94 <armin.ayoub94@gmail.com>
2022-06-08 17:17:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-quhb4x
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-quhb4x)
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { defineComponent, ref } from 'vue';
+import { isYesterday, addDays } from 'date-fns/esm';
+
+const onPanelChange = () => {
+  alert(`On panel change Triggered!!`);
+};
+</script>
+
+<template>
+  <n-calendar #="{ year, month, date }" :on-panel-change="onPanelChange">
+    {{ year }}-{{ month }}-{{ date }}
+  </n-calendar>
+</template>
